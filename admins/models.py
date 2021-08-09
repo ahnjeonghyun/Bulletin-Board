72f107abd26b12cc930bbc5557cca0eaab1c82d5
ahnjeonghyun <tgrf3090@gmail.com>
2021-08-09 17:05:44 +0900

    Refactor:모델수정

--- admins/models.py
+++ admins/models.py
@@ -5,6 +5,7 @@ class Admin(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     head       = models.CharField(max_length=300)
     hits       = models.IntegerField()
+    user_post  = models.OneToOneField('users.User', on_delete=models.SET_NULL, null=True)
 
     class Meta:
         db_table = 'admins'
