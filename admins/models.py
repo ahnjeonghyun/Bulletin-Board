100b8db004e34f993c7d6b281bfcfac7e563490a
ahnjeonghyun <tgrf3090@gmail.com>
2021-08-09 21:43:20 +0900

    Refactor:모델수정

--- admins/models.py
+++ admins/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Admin(models.Model):
     content    = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    head       = models.CharField(max_length=300)
+    title      = models.CharField(max_length=300)
     hits       = models.IntegerField()
     user_post  = models.OneToOneField('users.User', on_delete=models.SET_NULL, null=True)
 
