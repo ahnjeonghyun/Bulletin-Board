713ae8945968cb07f18100ef359bc2d149b7f67c
ahnjeonghyun <tgrf3090@gmail.com>
2021-08-24 17:59:06 +0900

    Add:Users Sign-in

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from users.views import UserPostView, UserPostDetailView, SignUpView
+from users.views import SignUpView, SignInView
 
 urlpatterns = [
     path('/sign-up',SignUpView.as_view()),
-    # path('/posts',UserPostView.as_view()),
+    path('/sign-in',SignInView.as_view()),
     # path('/posts/<int:post_num>',UserPostDetailView.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 import my_settings
 import bcrypt
 import json
+import jwt
 
 from django.shortcuts import render
 from django.http      import JsonResponse
@@ -37,10 +38,11 @@ class SignUpView(View):
             if not my_settings.PASSWORD.match(password):
                 return JsonResponse({'message':'INVALID_PASSWORD'},status=400)
 
+            hashed_pw = bcrypt.hashpw(password.encode('utf-8'),bcrypt.gensalt()).decode('utf-8')
             User.objects.create(
-                name = name,
-                password = password,
-                email = email,
+                name     = name,
+                password = hashed_pw,
+                email    = email,
                 nickname = nickname,
             )
             return JsonResponse({'message':'SUCCESS'},status=201)
@@ -48,96 +50,21 @@ class SignUpView(View):
         except KeyError:
             return JsonResponse({'message':'KEY_ERROR'},status=400)
 
-class UserPostView(View):
+class SignInView(View):
     def post(self,request):
         try:
-            data = json.loads(request.body)
-            name      = data['name']
-            nickname  = data['nickname']
-            password  = data['password']
-            content   = data['content']
-            title     = data['title']
-            secret_is = data.get('secret_is',False)
-            tag       = data.get('tag',None)
+            data     = json.loads(request.body)
+            password = data['password']
+            email    = User.objects.filter(email=data['email']).first()
 
-            if not name:
-                return JsonResponse({'message':'NOT_INPUT_NAME'},status=400)
-
-            if not nickname:
-                return JsonResponse({'message':'NOT_INPUT_NICKNAME'},status=400)
-
-            if not content:
-                return JsonResponse({'message':'NOT_INPUT_CONTENT'},status=400)
-
-            if not title:
-                return JsonResponse({'message':'NOT_INPUT_TITLE'},status=400)
-
-            if not my_settings.PASSWORD.match(password):
-                return JsonResponse({'message':'INVALID_PASSWORD'},status=400)
-
-            hash_pw = bcrypt.hashpw(password.encode('utf-8'),bcrypt.gensalt()).decode('utf-8')
-            User.objects.create(
-                name      = name,
-                nickname  = nickname,
-                password  = hash_pw,
-                content   = content,
-                title     = title,
-                secret_is = secret_is,
-                tag       = tag
-            )
-            return JsonResponse({'message':'SUCCESS'},status=201)
+            if not email:
+                return JsonResponse({'message':'INVALID_ID_ERROR'},status=401)
+            
+            if not bcrypt.checkpw(password.encode('utf-8'), email.password.encode('utf-8')):
+                return JsonResponse({'message':'INVALID_PW_ERROR'},status=401)
+            
+            encoded_jwt = jwt.encode({'user id': email.id}, my_settings.SECRET_KEY, algorithm = my_settings.ALGORITHM)
+            return JsonResponse({'message':encoded_jwt}, status = 200)
 
         except KeyError:
             return JsonResponse({'message':'KEY_ERROR'},status=400)
-
-    def get(self,request):
-        posts = User.objects.all().order_by('pk')
-        answer = [{
-            'no'           : post.pk,
-            'nickname'     : post.nickname,
-            'title'        : post.title,
-            'content'      : post.content,
-            'hits'         : post.hits,
-            'created_at'   : post.created_at,
-            'secret_is'    : post.secret_is,
-            'tag'          : post.tag,
-            'admin_asnwer' : {
-                            'no'               : Admin.objects.get(user_post_id=post).pk,
-                            'admin_title'      : Admin.objects.get(user_post_id=post).title,
-                            'admin_content'    : Admin.objects.get(user_post_id=post).content,
-                            'admin_hits'       : Admin.objects.get(user_post_id=post).hits,
-                            'admin_created_at' : Admin.objects.get(user_post_id=post).created_at } if post.admin_is_answer else None
-        }for post in posts ]
-
-        return JsonResponse({'data' : answer},status=200)
-
-class UserPostDetailView(View):
-    def get(self, request, post_num = None):
-        user  = User.objects.filter(pk = post_num)
-        admin = Admin.objects.filter(pk = post_num)
-
-        if not post_num:
-            return JsonResponse({'message':'INPUT_ERROR'},status=400)
-        
-        if user.exists():
-            post = user
-            post.first().hits += 1
-        
-        if admin.exists():
-            post = admin
-            post.first().hits += 1
-        
-        else:
-            return JsonResponse({'messsage':'THIS_PAGE_DOES_NOT_EXIST'},status=404)
-
-        data = {
-            'title'      : post.first().title,
-            'content'    : post.first().content,
-            'nickname'   : post.first().nickname,
-            'created_at' : post.first().created_at
-        }
-        post.save()
-
-        return JsonResponse({'data' : data},status=200)
-
-
